fc9547c2b2362fcd93ecae86d38b6e55380735b9
wwwrt <tpripon99@gmail.com>
2024-01-31 22:39:27 +0200

    text pentru about me

--- src/screens/AboutUs.vue
+++ src/screens/AboutUs.vue
@@ -1,5 +1,5 @@
-<script>
-
+<script setup>
+import MeText from '/src/components/AboutMeText.vue'
 </script>
 
 <style>
@@ -7,5 +7,9 @@
 </style>
 
 <template>
-    
+        <div class="flex mt-8 mb-8 ms-12 text-[1rem] md:text-[2rem] lg:text-[3rem] justify-center text-center text-[#d4a373] hover: font-bold ">Who are WE?</div>
+        <div class="flex flex-col lg:flex-row mb-20 items-center justify-center text-center lg:text-left">
+
+            <MeText />
+        </div>
 </template>
